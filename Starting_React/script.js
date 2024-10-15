26afa9ed01e0758354ffae69f770b9924f44e317
Aman Mittal <amanmittalma@gmail.com>
2024-10-15 23:30:32 +0530

    JSX and Babel

--- Starting_React/script.js
+++ Starting_React/script.js
@@ -1,5 +1,18 @@
-const heading = React.createElement(
-  "h1",
-  { className: "header" },
-  "Hello React"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { className: "header" },
+//   "Hello React"
+// );
+// ReactDOM.createRoot(document.getElementById("root")).render("heading");
+
+// const image = React.createElement(
+//   "img",
+//   { src: "https://files.codingninjas.in/coding-ninjas-24647.png" },
+//   null
+// );
+// console.log(image);
+
+/* React with JSX */
+
+const jsxHeading = <h1>Hello JSX</h1>;
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
