92ce8face490c148e84bf902afa0b3b196045087
Aman Mittal <amanmittalma@gmail.com>
2024-10-21 15:25:46 +0530

    Rendering Components using AND, OR and Ternary Operator

--- Learning JSX/script.js
+++ Learning JSX/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 // const sum = () => {
 //   const a = 10;
 //   const b = 20;
@@ -71,31 +71,136 @@
 
 /* Students Info Table */
 
-const studentsArr = [
-  { name: "std1", age: "10", marks: 20 },
-  { name: "std2", age: "12", marks: 22 },
-  { name: "std3", age: "14", marks: 24 },
-];
+// const studentsArr = [
+//   { name: "std1", age: 10, marks: 20 },
+//   { name: "std2", age: 12, marks: 22 },
+//   { name: "std3", age: 14, marks: 24 },
+// ];
 
-const Student = () => (
-  <>
-    <table border="2px">
-      <thead>
-        <th>Student Name</th>
-        <th>Student Age</th>
-        <th>Student Marks</th>
-      </thead>
-      <tbody>
-        {studentsArr.map((student, i) => (
-          <tr>
-            <td>{student.name}</td>
-            <td>{student.age}</td>
-            <td>{student.marks}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </>
-);
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<Student />);
+// const Student = () => (
+//   <>
+//     <h1>Students Details</h1>
+//     <table border="2px">
+//       <thead>
+//         <th>Student Name</th>
+//         <th>Student Age</th>
+//         <th>Student Marks</th>
+//       </thead>
+//       <tbody>
+//         {studentsArr.map((student, i) => (
+//           <tr>
+//             <td>{student.name}</td>
+//             <td>{student.age}</td>
+//             <td>{student.marks}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   </>
+// );
+
+// rootElement.render(<Student />);
+
+/* Inventory */
+// const SoldItems = () => {
+//   const itemsSold = [
+//     { id: 1, name: "iPhone 14", price: 1200, qty: 22 },
+//     { id: 2, name: "iPad Pro", price: 800, qty: 18 },
+//     { id: 3, name: "Macbook Air", price: 1500, qty: 7 },
+//     { id: 4, name: "Samsung S23", price: 1100, qty: 16 },
+//     { id: 5, name: "Dell Inspiron 5590", price: 1200, qty: 5 },
+//   ];
+
+//   return (
+//     <>
+//       <h1>Record of sold items</h1>
+//       <table border="1px">
+//         <thead>
+//           <th>Id</th>
+//           <th>Name</th>
+//           <th>Selling price</th>
+//           <th>Quantity</th>
+//         </thead>
+//         <tbody>
+//           {itemsSold.map((item, i) => (
+//             <tr key={i}>
+//               <td>{item.id}</td>
+//               <td>{item.name}</td>
+//               <td>{item.price}</td>
+//               <td>{item.qty}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//         <tfoot>
+//           <td>Total Revenue</td>
+//           <td colspan="4">$5800</td>
+//         </tfoot>
+//       </table>
+//     </>
+//   );
+// };
+
+// const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+// rootElement.render(<App />);
+
+/* Rendering Components Based on Condition */
+// const App = () => {
+//   let showStudent = true;
+//   if (showStudent) {
+//     return <Student />;
+//   }
+//   return <SoldItems />;
+// };
+
+// rootElement.render(<App />);
+
+/* Rendering Components Based on Ternary Operator */
+// const loggedIn = true;
+// const Login = () => (
+//   <>
+//     <h3>Login to Continue</h3>
+//     <form>
+//       <input placeholder="email" />
+//       <input placeholder="password" />
+//       <button>Login</button>
+//     </form>
+//   </>
+// );
+
+// const Home = () => <h1>Hello User!</h1>;
+// const App = () => {
+//   return (
+//     <>
+//       {/* <h1>Hello {loggedIn ? "Alex" : "User"}!</h1> */}
+//       {loggedIn ? <Login /> : <Home />}
+//     </>
+//   );
+// };
+
+// rootElement.render(<App />);
+
+/* Conditional Rendering using AND operator */
+// const App = () => {
+//   const loggedIn = false;
+//   return (
+//     <>
+//       <h1>Hello {loggedIn ? "Alex" : "User"}!</h1>
+//       {loggedIn && <p>Welcome to Portal </p>}
+//     </>
+//   );
+// };
+// rootElement.render(<App />);
+
+/* Conditional Rendering using OR Operator */
+const App = () => {
+  const loggedIn = true;
+  const firstName = "Alexa";
+  const lastName = "Johnson";
+  return (
+    <>
+      <h1>Hello {loggedIn ? lastName || firstName : "User"}!</h1>
+      {loggedIn && <p>Welcome to Portal </p>}
+    </>
+  );
+};
+rootElement.render(<App />);
